a6ebf3d621a2973f64e7478ab0933f8e65aae72d
akouvivien <akouvivien@gmail.com>
2023-01-11 18:14:16 +0000

    first commit ExerciceXP Week4 Day1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { GreetingComponent } from './greeting/greeting.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    GreetingComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
